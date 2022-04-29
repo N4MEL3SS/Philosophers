b5130fd88153a7d7c0c7751d42493df20582646e
celadia <celadia@student.school-21.ru>
2022-04-29 07:37:04 +0300

    repair

--- philo/sources/thread.c
+++ philo/sources/thread.c
@@ -2,6 +2,8 @@
 
 int	make_thread(t_all *info, int i)
 {
+	info->phil[i].start_time = ft_get_time();
+	info->phil[i].stop_eat = info->phil[i].start_time;
 	if (pthread_create(&info->phil[i].thread, NULL, \
 		&start_act, &info->phil[i]) != 0)
 		return (INDEX_THREAD);
@@ -17,18 +19,13 @@ int	thread_init(t_all *info, int f1, int f2)
 	error = 0;
 	while (i < info->data->num_phil - f1)
 	{
-		info->phil[i].start_time = ft_get_time();
-		info->phil[i].stop_eat = info->phil[i].start_time;
 		error += make_thread(info, i);
 		i += 2;
 	}
 	i = 1;
-	usleep(50);
-	while (++i < info->data->num_phil - f2)
+	while (i < info->data->num_phil - f2)
 	{
-		info->phil[i].start_time = ft_get_time();
-		info->phil[i].stop_eat = info->phil[i].start_time;
-		error += make_thread(info, ++i);
+		error += make_thread(info, i);
 		i += 2;
 	}
 	return (error);
@@ -39,6 +36,7 @@ int	phil_thread_init(t_all *info)
 	if (thread_init(info, (info->data->num_phil - 1) % 2, \
 	info->data->num_phil % 2))
 		return (INDEX_THREAD);
+	usleep(1000);
 	if (pthread_create(&info->death, NULL, &check_dead, info) != 0)
 		return (INDEX_THREAD);
 	if (pthread_join(info->death, NULL) != 0)
